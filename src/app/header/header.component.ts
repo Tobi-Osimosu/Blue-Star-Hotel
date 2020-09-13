da924d05c8757359ce0d13193dea57b5bee66bf0
Tobi Osimosu <osimosutobi@gmail.com>
2020-09-13 16:29:01 +0100

    update

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject('windowObject') private window: Window
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.window.addEventListener('resize', () => {
+      if (matchMedia('(min-width: 992px)').matches) {
+        this.closeMenu();
+      }
+    });
   }
 
+  openMenu(event: Event) {
+    const navButton = this.document.querySelector('.menu-wrapper');
+    const navMenu = this.document.querySelector('.navLinks');
+
+    navButton.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    console.log('yeah');
+
+    event.preventDefault();
+    event.stopImmediatePropagation();
+  }
+
+  closeMenu() {
+    const navButton = this.document.querySelector('.menu-wrapper');
+    const navMenu = this.document.querySelector('.navLinks');
+
+    if (navButton.classList.contains('active')) {
+      navButton.classList.remove('active');
+      navMenu.classList.remove('active');
+    }
+    console.log('yyysudb');
+  }
 }
